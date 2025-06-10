ac4c652fdc7e16e12d06cdc99456ea5b1d50d4f8
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2025-06-10 17:38:53 +0800

    🐛Fix the console compile bug.

--- Tests/ArchiToolkit.Console/TestValidator.cs
+++ Tests/ArchiToolkit.Console/TestValidator.cs
@@ -1,5 +1,4 @@
 ﻿using ArchiToolkit.ValidResults;
-using FluentValidation;
 
 namespace ArchiToolkit.Console;
 
@@ -11,18 +10,6 @@ public partial class SpanFormatResult
 [GenerateValidResult<DateTime>]
 public partial class DateTimeResult
 {
-    public static ValidResult<bool> TryFormat(
-        ValidResult<DateTime> self,
-        Span<char> destination, out ValidResult<int> charsWritten,
-        ReadOnlySpan<char> format = default,
-        ValidResult<IFormatProvider>? provider = null,
-        [System.Runtime.CompilerServices.CallerArgumentExpression("self")] string selfName = "",
-        [System.Runtime.CompilerServices.CallerArgumentExpression("provider")] string providerName = "",
-        [System.Runtime.CompilerServices.CallerFilePath] string _filePath = "",
-        [System.Runtime.CompilerServices.CallerLineNumber] int _fileLineNumber = 0)
-    {
-        provider ??= ValidResult<IFormatProvider>.Data.Ok((IFormatProvider)null);
-    }
 }
 
 [GenerateValidResult<TestClass2>]
